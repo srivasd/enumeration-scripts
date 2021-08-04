80fdf52607e4832269c69d2445ab3bb91844865d
Sergio Rivas <s.rivasd@alumnos.urjc.es>
2021-08-04 11:39:38 +0200

    Update portdiscovery.sh

--- portdiscovery.sh
+++ portdiscovery.sh
@@ -3,7 +3,7 @@
 ip=X.X.X.X
 port=1
 while [ $port -lt 1000 ]; do
-echo "Trying with port" $port "for the ip" $ip
-timeout 1 bash -c "echo '' > /dev/tcp/$ip/$port" && echo "Found: " $port " open!"
+# echo "Trying with port" $port "for the ip" $ip
+timeout 1 bash -c "echo '' > /dev/tcp/$ip/$port" > /dev/null 2>&1 && echo "Found: " $port " open!"
 port=$((port+1))
 done
